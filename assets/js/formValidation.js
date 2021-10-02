5331d43ac3cd530940882b09e3c09b77cc1713d2
Lewis Barton <lewis@Lewiss-MacBook-Air.local>
2021-10-02 16:19:58 +0100

    FEAT: formValidation.js - Added func to check how soon input date is, updateWeather func now fully working.

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -7,7 +7,7 @@ const dayData = {
 };
 
 form.addEventListener('submit', function (event) {
-  const date = getDate();
+  const date = userDate();
 
   if (!checkDate(date)) {
     dateError(date);
@@ -15,7 +15,7 @@ form.addEventListener('submit', function (event) {
   }
 });
 
-function getDate() {
+function userDate() {
   const form = document.getElementsByTagName('form')[0];
   const date = new Date(form.date.value);
 
@@ -58,6 +58,19 @@ function dateError(invalidDate) {
     ' is invalid. Please ensure that the date is within 7 days of today';
 }
 
+function inputFromToday() {
+  const input = userDate();
+  const today = new Date();
+  const inputNum = input.getDate();
+  const todayNum = today.getDate();
+  var daysToDate = '';
+
+  if (checkDate(input)) {
+    daysToDate = inputNum - todayNum;
+    return daysToDate;
+  }
+}
+
 function getLocations() {
   const locationIds = [
     'locationOne',
@@ -85,7 +98,7 @@ function getLocations() {
       const longitude = places.geometry.location.lng();
 
       getTidal(placeName);
-      getWeather(longitude, latitude, currentLocation);
+      getWeather(longitude, latitude, currentLocation, placeName);
     });
   });
 }
@@ -98,7 +111,7 @@ async function getTidal(tidalTown) {
     cache: 'no-cache',
     headers: {
       'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': 'a1550ee5d52542f0a2b82bada9e975a7',
+      'Ocp-Apim-Subscription-Key': 'b91e97479a2b466eabc281d3497b8883',
     },
     redirect: 'follow',
     referrerPolicy: 'no-refferer',
@@ -111,27 +124,36 @@ async function getTidal(tidalTown) {
     .catch((err) => console.log(err));
 }
 
-function getWeather(lng, lat, inputId) {
+function getWeather(lng, lat, inputId, placeName) {
   const key = 'bbc573251f7231d889f8506528105528';
   const exclusions = 'current,minutely,hourly,alerts';
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=${exclusions}&appid=${key}`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=${exclusions}&appid=${key}&units=metric`;
 
   fetch(url)
-    .then(function (response) {
-      // Returns API call in json format to be manipulated
-      return response.json();
-    })
-    .then(function (data) {
+    .then((response) => response.json())
+    .then(function (returnedData) {
       // Data to be passed to update day data
-      updateWeather(data, inputId);
+      updateWeather(returnedData, inputId, lng, lat, placeName);
     })
-    .catch(function (error) {
-      console.log(error);
-    });
+    .catch((error) => console.log(error));
 }
 
-function updateWeather(weather, locationId) {
-  const date = getDate();
+function updateWeather(weather, locationId, lng, lat, placeName) {
+  const dayIndex = inputFromToday();
+  weather = weather.daily[dayIndex];
+
+  for (let i = 0; i < 4; i++) {
+    dayData[locationId].thisLocation = placeName;
+    dayData[locationId].lng = lng;
+    dayData[locationId].lat = lat;
+    dayData[locationId].temp = weather.temp.day;
+    dayData[locationId].feelsLike = weather.feels_like;
+    dayData[locationId].wind = weather.wind_speed;
+    dayData[locationId].cloud = weather.clouds;
+    dayData[locationId].chanceOfRain = weather.pop;
+    dayData[locationId].rain = weather.rain;
+    dayData[locationId].snow = weather.snow;
+  }
 }
 
 window.onload = function () {
